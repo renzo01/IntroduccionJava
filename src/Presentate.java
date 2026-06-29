572f479df38b9eb8e0bf579816641d901b4f3aad
Renzo Gomez <renzogomez577@gmail.com>
2026-06-29 12:01:13 -0500

    end of introduccion

---
+++ src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre : Renzo Manuel Gomez cesare");
+        System.out.println("Edad : 26");
+        System.out.println("Nacionalidad : Peruano 🇵🇪");
+    }
+}
